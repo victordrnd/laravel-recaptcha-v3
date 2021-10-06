2358b068bf33a9861710b658740d7dfc41735493
Victor Durand <vic20016@gmail.com>
2021-10-06 19:35:49 +0200

    Update ReCaptcha.php

--- src/Middleware/ReCaptcha.php
+++ src/Middleware/ReCaptcha.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Huangdijia\Recaptcha\Middleware;
+namespace Victordrnd\Recaptcha\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
@@ -37,25 +37,6 @@ class ReCaptcha
             }
         }
 
-        if (config('recaptcha-v2.enable', true)) {
-
-            $hostname  = $hostname ?: $request->getHost();
-            $recaptcha = app('recaptcha-v2')->setExpectedHostname($hostname);
-
-            if ($action) {
-                $recaptcha->setExpectedAction($action);
-            }
-
-            if ($score) {
-                $recaptcha->setScoreThreshold($score);
-            }
-
-            $resp = $recaptcha->verify($request->input(config('recaptcha-v2.input_name', 'g-recaptcha-response')), $request->getClientIp());
-
-            if (!$resp->isSuccess()) {
-                abort(config('recaptcha-v2.response_code', 401), config('recaptcha-v2.response_message', 'Google ReCaptcha Verify Fails'));
-            }
-        }
 
         return $next($request);
     }
