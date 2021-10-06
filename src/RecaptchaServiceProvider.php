fda81b8123990259d374b0320f4de08133ea346b
Victor Durand <vic20016@gmail.com>
2021-10-06 19:35:30 +0200

    Update RecaptchaServiceProvider.php

--- src/RecaptchaServiceProvider.php
+++ src/RecaptchaServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Huangdijia\Recaptcha;
+namespace Victordrnd\Recaptcha;
 
 use Illuminate\Container\Container;
 use Illuminate\Support\Facades\Blade;
@@ -22,21 +22,6 @@ class RecaptchaServiceProvider extends ServiceProvider
     {
         $this->bootConfig();
 
-        // v2
-        $this->app['validator']->extend('recaptcha-v2', function ($attribute, $value, $parameters, $validator) {
-            $app       = Container::getInstance();
-            $recaptcha = $app['recaptcha-v2']->setExpectedHostname($app['request']->getHost());
-
-            if ($parameters[0] ?? '') {
-                $recaptcha->setExpectedAction($parameters[0]);
-            }
-
-            if ($parameters[1] ?? '') {
-                $recaptcha->setScoreThreshold($parameters[1]);
-            }
-
-            return $recaptcha->verify($value, $app['request']->getClientIp())->isSuccess();
-        });
 
         // v3
         $this->app['validator']->extend('recaptcha', function ($attribute, $value, $parameters, $validator) {
@@ -69,23 +54,8 @@ class RecaptchaServiceProvider extends ServiceProvider
             return $recaptcha->verify($value, $app['request']->getClientIp())->isSuccess();
         });
 
-        $this->loadViewsFrom(__DIR__ . '/../views', 'recaptcha');
 
-        // @recapcha_initjs(['site_key' => 'xxx', 'action' => 'action_name']);
-        Blade::directive('recaptcha_initjs', function ($expression) {
-            $expression = Blade::stripParentheses($expression) ?: '[]';
-            $path       = 'recaptcha::components.initjs';
-
-            return "<?php echo \$__env->make('{$path}', {$expression}, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>";
-        });
-
-        // @recapcha_field(['site_key' => 'xxx', 'name' => 'input_name']);
-        Blade::directive('recaptcha_field', function ($expression) {
-            $expression = Blade::stripParentheses($expression) ?: '[]';
-            $path       = 'recaptcha::components.field';
-
-            return "<?php echo \$__env->make('{$path}', {$expression}, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>";
-        });
+     
     }
 
     /**
@@ -94,14 +64,11 @@ class RecaptchaServiceProvider extends ServiceProvider
     protected function bootConfig()
     {
         $configV3 = __DIR__ . '/../config/recaptcha-v3.php';
-        $configV2 = __DIR__ . '/../config/recaptcha-v2.php';
 
         $this->mergeConfigFrom($configV3, 'recaptcha-v3');
-        $this->mergeConfigFrom($configV2, 'recaptcha-v2');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([$configV3 => $this->app->basePath('config/recaptcha-v3.php')], 'config');
-            $this->publishes([$configV2 => $this->app->basePath('config/recaptcha-v2.php')], 'config');
         }
     }
 
@@ -115,9 +82,6 @@ class RecaptchaServiceProvider extends ServiceProvider
             return new ReCaptcha($app['config']['recaptcha-v3.secret_key']);
         });
 
-        $this->app->bind('recaptcha-v2', function ($app) {
-            return new ReCaptcha($app['config']['recaptcha-v2.secret_key']);
-        });
 
         // alias
         $this->app->alias('recaptcha-v3', 'recaptcha');
@@ -132,7 +96,6 @@ class RecaptchaServiceProvider extends ServiceProvider
         return [
             'recaptcha',
             'recaptcha-v3',
-            'recaptcha-v2',
         ];
     }
 }
